fd41be8ac48fb11a6ca25ac9ab65dae9204a3d84
Maxang123 <132574802+Maxang123@users.noreply.github.com>
2024-08-05 18:26:12 +0700

    Delete GenQR.py

--- GenQR.py
+++
@@ -1,9 +0,0 @@
-import qrcode   
-
-
-print("\n***** QR Generrator *****\n")
-data = input("Enter Message or URL:")
-imgName = input("Enter Image Name To Save:")
-qrimg = qrcode.make(data)
-qrimg.save(imgName+".png")
-print("\nQR Code Generrated")
